c645811f2b5528dc370e5f35a3469ef8722a35d4
Yang Tang <pureyang@gmail.com>
2012-11-13 00:47:04 -0800

    distributed.key and crawler.reg are expected in app/crawler dir

--- src/crawler/src/java/com/leancog/crawl/RegistrationCheck.java
+++ src/crawler/src/java/com/leancog/crawl/RegistrationCheck.java
@@ -3,6 +3,7 @@ package com.leancog.crawl;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
@@ -12,7 +13,6 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 import javax.crypto.Cipher;
 
@@ -24,7 +24,9 @@ import com.leancog.Salesforce.UtilityLib;
 public class RegistrationCheck {
   private static final Logger LOG = LoggerFactory.getLogger(RegistrationCheck.class);
   private static final String crawlerRegistrationFileName = "crawler.reg";
-  private final String xform = "RSA";
+  private static final String distributedKeyFileName = "distributed.key";
+  private final String ENCRYPTION_ALGORITHM = "RSA";
+  private final int ENCRYPTION_LENGTH = 4096;
   
 	public RegistrationCheck() {
 		// upon creation, always check 
@@ -33,20 +35,29 @@ public class RegistrationCheck {
 	public boolean verify() {
 		
 	  // find path to serial file
-	  //ClassLoader classLoader = RegistrationCheck.class.getClassLoader();
-	  //File classpathRoot = new File(classLoader.getResource("").getPath());
-
+	  
 	  RandomAccessFile f;
     try {
-      // read registration file
-      PrivateKey prvk = LoadEncryptionKey(".", xform);
+      // read key file
+      File parentFile = new File(distributedKeyFileName);
+      // hack to get to the right directory
+      String reg = parentFile.getAbsolutePath().replace("bin", "crawlers");
+      File keyFile = new File(reg);
+      LOG.info("Salesforce Crawler:Looking for key file="+keyFile.getAbsolutePath());
+      PrivateKey prvk = LoadEncryptionKey(keyFile, ENCRYPTION_ALGORITHM);
 
       // read the registration file for verification
-      f = new RandomAccessFile(new File("./"+crawlerRegistrationFileName), "r");  
+      parentFile = new File(crawlerRegistrationFileName);
+      // hack to get to the right directory
+      reg = parentFile.getAbsolutePath().replace("bin", "crawlers");
+      File registrationFile = new File(reg);
+      LOG.info("Salesforce Crawler:Looking for registration file="+registrationFile.getAbsolutePath());
+      
+      f = new RandomAccessFile(registrationFile, "r");
       
       byte[] encBytesFromFile = new byte[(int)f.length()];
       f.readFully(encBytesFromFile);
-      byte[] decBytes = decrypt(encBytesFromFile, prvk, xform);
+      byte[] decBytes = decrypt(encBytesFromFile, prvk, ENCRYPTION_ALGORITHM);
 
       f.close();
       
@@ -69,7 +80,7 @@ public class RegistrationCheck {
     } catch (Exception e) {
       UtilityLib.errorException(LOG, e);
     }
-
+	  
 		// for now always return true
 		return false;
 	}
@@ -81,16 +92,35 @@ public class RegistrationCheck {
     return cipher.doFinal(inpBytes);
   }
   
-  public PrivateKey LoadEncryptionKey(String path, String algorithm) 
+  public PrivateKey LoadEncryptionKey(File keyFile, String algorithm) 
+      throws IOException, NoSuchAlgorithmException,
+      InvalidKeySpecException {
+    // Read Private Key.
+    File filePrivateKey = keyFile;
+    FileInputStream fis = new FileInputStream(keyFile);
+    byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
+    fis.read(encodedPrivateKey);
+    fis.close();
+
+    // Generate PrivateKey
+    KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+    PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(
+        encodedPrivateKey);
+    PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+
+    return privateKey;
+    }
+/*
+  public PrivateKey LoadEncryptionKey(String algorithm) 
       throws IOException, NoSuchAlgorithmException,
       InvalidKeySpecException {
       // Read Private Key.
-      File filePrivateKey = new File(path + "/distributed.key");
-      LOG.info("Salesforce Crawler:Looking for key file="+filePrivateKey.getAbsolutePath());
-      FileInputStream fis = new FileInputStream(path + "/distributed.key");
-      byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
-      fis.read(encodedPrivateKey);
-      fis.close();
+      InputStream is = this.getClass().getResourceAsStream("/"+distributedKeyFileName);
+      long length = is.skip(Long.MAX_VALUE);
+      byte[] encodedPrivateKey = new byte[(int)length];
+      is = this.getClass().getResourceAsStream("/"+distributedKeyFileName);
+      is.read(encodedPrivateKey);
+      is.close();
   
       // Generate PrivateKey
       KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
@@ -100,5 +130,5 @@ public class RegistrationCheck {
   
       return privateKey;
     }
-
+*/  
 }
